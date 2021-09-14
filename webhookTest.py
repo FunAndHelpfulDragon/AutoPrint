e56c93dd2a8fd7f6a85e1337177db9790482ac03
Dragmine <dan@thebanners.uk>
2021-09-14 23:03:32 +0100

    Update webhookTest.py

--- webhookTest.py
+++ webhookTest.py
@@ -3,8 +3,9 @@ from discord_webhook import DiscordWebhook
 
 class webhook:
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self):
+        with open("webhook.txt", 'r') as f:
+            self.url = f.read().strip()
 
     def SendMessage(self, message):
         webhook = DiscordWebhook(url=self.url, content=message)
